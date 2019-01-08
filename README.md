1ed1d91c338e2a1b0b6894da1d9f2ca3b31d27df
salva116ario <salvatore.sancesario.lp2@gmail.com>
2019-01-08 14:55:45 +0100

    readme.md added

--- README.md
+++ README.md
@@ -2,15 +2,41 @@
 
 ### Description :
 
-...
+Création thème enfant du thème Astra
+pour répondre au cahier des charges site de Raoul
+https://webtravel.space/bt2
 
 ### Installation :
 
-...
+Installation du thème parent astra sur Wordpress
+
+Récupération struture de base du thème enfant astra-child
+
+Import en format .zip du thème enfant Astra sur Wordpress
+
+Modification des fichiers .css et .php
+style.css pour relier astra-child à son thème parent
+functions.php pour prendre en compte la feuille de style du thème enfant
 
 ### Utilisation :
 
-...
+Personnalisation en fonction de la charte graphique
+
+functions.php
+Définition de la palette choisie
+
+style.css
+Ajout des classes correspondant aux couleurs des textes et des fonds
+Ajout d'une classe sponsor dédiée à l'affichage des logos des partenanires sur la page accueil
 
 
 ### Ressources :
+
+Thème parent Astra
+https://fr.wordpress.org/themes/astra/
+
+Thème Enfant Astra
+https://github.com/pehaa/astra-child
+
+lien charte graphique
+https://company-45185.frontify.com/d/fVRpz7xGUu30/blog-trotter-style-guide
--- functions.php
+++ functions.php
@@ -1,1 +1,44 @@
 <?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+add_action( 'after_setup_theme', 'mytheme_setup_theme_supported_features' );
+
+function my_theme_enqueue_styles() {
+ 
+    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+ 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+
+
+function mytheme_setup_theme_supported_features() {
+    add_theme_support( 'editor-color-palette', array(
+        array(
+            'name' => __( 'white', 'themeLangDomain' ),
+            'slug' => 'white',
+            'color' => '#fff',
+        ),
+        array(
+            'name' => __( 'MineShaft', 'themeLangDomain' ),
+            'slug' => 'mineshaft',
+            'color' => '#4A4A4A',
+        ),
+        array(
+            'name' => __( 'Mongoose', 'themeLangDomain' ),
+            'slug' => 'mongoose',
+            'color' => '#B09F82',
+        ),
+        array(
+            'name' => __( 'Pickled Bean', 'themeLangDomain' ),
+            'slug' => 'pickled-bean',
+            'color' => '#8B572A',
+        ),
+    ) );
+}
+
+
+?>
